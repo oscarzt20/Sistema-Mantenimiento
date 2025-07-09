3858832e3121a223c49cc3b7de9be5ab03aa496f
debainilla1 <debanhiabigaillibradoalmada@hotmail.com>
2025-07-09 13:42:01 -0600

    bd

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; // Usuario por defecto en XAMPP
 $password = ""; // Contraseña por defecto en XAMPP (vacía)
-$dbname = "mantenimiento";
+$dbname = "mantenimientobd";
 
 // Crear conexión
 $conexion = new mysqli($servername, $username, $password, $dbname);
